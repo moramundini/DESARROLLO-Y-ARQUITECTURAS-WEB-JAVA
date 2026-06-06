7e143992934d07041f39ec535c75ae809645f421
Mora Mundini <mora.mundini@alumnos.uai.edu.ar>
2026-06-06 12:57:04 -0300

    Agrego

---
+++ Clase8/exercises/variables_and_operators.js
@@ -0,0 +1,21 @@
+/*Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
+var num1 = 1;
+var num2 = 2;
+var resultado = num1 + num2;
+console.log(resultado);
+
+
+/*Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+var txt1 = "Hola";
+var txt2 = " a todos";
+var resultado = txt1 + txt2;
+console.log(resultado);
+
+
+/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+Editor de texto*/
+var txt1 = "Hola";
+var txt2 = " a todos";
+
+var resultado = txt1.length + txt2.length;
+console.log(resultado);
