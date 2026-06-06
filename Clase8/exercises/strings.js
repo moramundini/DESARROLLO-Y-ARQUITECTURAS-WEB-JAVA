09847c9f5bb6933bc55b649a3486d0b975e2e99b
Mora Mundini <mora.mundini@alumnos.uai.edu.ar>
2026-06-05 23:20:15 -0300

    Agrego

---
+++ Clase8/exercises/strings.js
@@ -0,0 +1,30 @@
+/*Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).*/
+var text;
+text= "One of the best";
+text.toUpperCase();
+console.log(text.toUpperCase());
+
+
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+var text2;
+text2= "Number of the beast";
+text2.substring (0,4);
+console.log(text2.substring (0,4));
+
+
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+var text3,result;
+text3= "Back in time";
+result = text3.substring (9);
+console.log(result);
+
+
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable 
+(utilizar substring, toUpperCase, toLowerCase y el operador +).*/
+
+
+/*Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+
+
+/*Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores 
+para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).*/
