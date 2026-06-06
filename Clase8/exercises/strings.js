7a56045e3c7fc52c655ea18e226cbef57aa5fc8a
Mora Mundini <mora.mundini@alumnos.uai.edu.ar>
2026-06-06 14:20:18 -0300

    Agrego

--- Clase8/exercises/strings.js
+++ Clase8/exercises/strings.js
@@ -21,10 +21,25 @@ console.log(result);
 
 /*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable 
 (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
+var texto = "pROGRAMACION";
+let resultado = texto.substring(0,1).toUpperCase() + texto.substring(1).toLowerCase();
+console.log(resultado); 
 
 
 /*Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+var texto = "Hola mundoJavaS";
+var posicionEspacio = texto.indexOf(" ");
+console.log(posicionEspacio); 
 
 
 /*Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores 
 para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).*/
+var texto = "programacionJAVa desarrolloSOFTWARE"; 
+var espacio = texto.indexOf(" ");
+
+var palabra1 = texto.substring(0,1).toUpperCase() + texto.substring(1,espacio).toLowerCase();
+
+var palabra2 = texto.substring(espacio+1,espacio+2).toUpperCase() + texto.substring(espacio+2).toLowerCase();
+
+var resultado = palabra1 + " " + palabra2;
+console.log(resultado); 
